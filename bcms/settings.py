1abe4b35858f740a02638e692628ea4c467a85d0
cerbercerber <bertrand.cerede@orange.fr>
2018-10-18 16:28:02 +0200

    static

--- bcms/settings.py
+++ bcms/settings.py
@@ -161,7 +161,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 
 #pythonanywhere
-STATIC_ROOT = "/home/cerber/bcms/bo/static"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
